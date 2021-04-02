83448374c7de9be0bb07af6c4e76c60c0a9fb7d3
Henry Toti <henryokoth3@gmail.com>
2021-04-02 17:10:53 +0300

    Update postReducer.js

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,21 +1,37 @@
-import { FETCH_POSTS, NEW_POST } from '../actions/types';
+import { FETCH_POSTS, NEW_POST, SEARCH_POST } from "../actions/types";
 
 const initialState = {
   items: [],
-  item: {}
+  item: {},
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case FETCH_POSTS:
       return {
         ...state,
-        items: action.payload
+        items: action.payload,
       };
     case NEW_POST:
       return {
         ...state,
-        item: action.payload
+        item: action.payload,
+        items: [...state.items, action.payload],
+      };
+    case SEARCH_POST:
+      let value = action.payload;
+
+      let filteredValues = state.items.filter((itema) => {
+        if (value) {
+          return itema.title.toLowerCase().includes(value);
+        } else {
+          return FETCH_POSTS;
+        }
+      });
+
+      return {
+        ...state,
+        items: filteredValues,
       };
     default:
       return state;
