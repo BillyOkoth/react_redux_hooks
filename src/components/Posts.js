5765790e04eab2f2c377c0d9eff4eb88d1b72226
Henry Toti <henryokoth3@gmail.com>
2021-04-02 17:09:35 +0300

    Update Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,44 +1,107 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchPosts } from '../actions/postActions';
+import React, { Component, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ReactPaginate from 'react-paginate';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
 
-class Posts extends Component {
-  componentWillMount() {
-    this.props.fetchPosts();
-  }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.newPost) {
-      this.props.posts.unshift(nextProps.newPost);
-    }
-  }
+const Posts = () => {
+  //get the data from the redux store using the useSelector.
+  const posts = useSelector((state) => state.posts); 
 
-  render() {
-    const postItems = this.props.posts.map(post => (
+  console.log('posts',posts);
+
+  
+
+  //filter using the hooks state and not using reducer.
+  //1. search is the even.target value that is captureed on change.
+  //2 we set search using the setSerch Method i.e setSearch(event.target.value) this is equivalent to search.
+
+  
+  const [search ,setSearch] = useState("");   
+  
+  //4.we set the state of the filteresd posts using setFilteredPost 
+  //from the useEffect method we set the state to be filteredPosts.
+  //then we use the filterdPosts to map the data.
+
+  const [filteredPosts,setFilteredPosts] = useState([]);  
+  ///this is for pagination.
+  const [offset,setOffset] =  useState(0);
+  
+  const [perPage] = useState(20);
+  const [pageCount,setPageCount] = useState(0);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    const loadPosts = async () => {
+      await dispatch(fetchPosts());
+    };
+
+    loadPosts();
+
+
+  }, []);
+
+  useEffect(() => {
+    setFilteredPosts(
+      posts.items.filter((post) =>
+        post.title.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+    setPageCount(Math.ceil(filteredPosts.length/perPage));
+  }, [search, posts]);
+
+
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage + 1)
+};
+
+const slice = filteredPosts.slice(offset,offset + perPage);
+console.log('sliece',slice);
+
+const postItems = slice.map((post) => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
-      </div>
-    ));
-    return (
+      </div> 
+    )
+    
+    ).reverse();
+    // setData(postItems)
+    // setPageCount(Math.ceil(filteredPosts.length/perPage));
+  return (
+    <div>
+      search
       <div>
-        <h1>Posts</h1>
-        {postItems}
+        <input
+          type="text"
+          name="search"
+          placeholder="Search"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
       </div>
-    );
-  }
-}
-
-Posts.propTypes = {
-  fetchPosts: PropTypes.func.isRequired,
-  posts: PropTypes.array.isRequired,
-  newPost: PropTypes.object
+      <hr></hr>
+      <h1>Posts</h1>
+      {postItems}
+      <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
+    </div>
+  );
 };
 
-const mapStateToProps = state => ({
-  posts: state.posts.items,
-  newPost: state.posts.item
-});
-
-export default connect(mapStateToProps, { fetchPosts })(Posts);
+export default Posts;
